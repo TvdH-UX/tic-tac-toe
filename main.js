03b27e49a6728c4b4b09a836d7eb6932c723168c
TvdH-UX <thomasheuvel7@gmail.com>
2022-08-22 14:27:22 +0200

    Progress save other workstation

---
+++ main.js
@@ -0,0 +1,16 @@
+// Store the gameboard inside an object
+// Use a module
+let gameBoard = (function() {
+  let board = [
+    ["","",""],
+    ["","",""],
+    ["","",""]
+  ]
+})();
+
+// Store the players inside objects
+// Use a factory
+
+// An object to control the flow of the game
+// Use a module
+// Whose turn is it
