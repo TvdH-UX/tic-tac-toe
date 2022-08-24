3a1ac77b2e2b4360a4f18b3e7a8975aab3325467
TvdH-UX <thomasheuvel7@gmail.com>
2022-08-24 15:35:02 +0200

    Add html layout and css styling

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
+// Search the DOM once for a certain module
+
+
 // Store the gameboard inside an object
 // Use a module
+// Create a variable containing the board as an array
+// Render the board at the start
+
+// Rerender the board after every move
 let gameBoard = (function() {
   let board = [
-    ["","",""],
-    ["","",""],
-    ["","",""]
-  ]
+    ["X","X","O"],
+    ["O","O","X"],
+    ["X","O","X"]
+  ];
+
+  function render() {
+    console.log(board);
+  };
+
+  render();
 })();
 
 // Store the players inside objects
